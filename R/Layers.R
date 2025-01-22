da34b154aff728c53a86c8560b29aca107416119
LarsELund <larselnd@gmail.com>
2025-01-22 14:08:21 +0100

    add layers

---
+++ R/Layers.R
@@ -0,0 +1,129 @@
+library(torch)
+
+
+#' Generate prior inclusion probabilities for a LBBNN layer
+#' @description A function to generate prior probability values for 
+#' each weight in an LBBNN layer. Currently only the same probability for each
+#' weight in a layer is allowed. 
+#' @param x A number between 0 and 1.
+#' @param out_shape number of output neurons.
+#' @param in_shape number of input neurons.
+#' @return a torch tensor with shape (out_shape,in_shape) with prior probabilities.
+#' @examples
+#' alpha_prior(0.25, 10,5)
+alpha_prior <- function(x,out_shape,in_shape,device) {
+  alpha_out <- torch_zeros(out_shape,in_shape,device=device)
+  if (!is.numeric(x) )
+    stop("invalid_class:", " alpha must be numeric")
+  if (any(x <=0) | any(x>=1))
+    stop("invalid_value:", " alpha must be between 0 and 1")
+  
+  
+  if (length(x) != 1)
+    stop("only one value of alpha allowed per layer")
+  
+  return(alpha_out + x)
+  
+}
+
+
+
+#' Class to generate an LBBNN feed forward layer
+#' @param in_features number of input neurons.
+#' @param out_features number of output neurons.
+#' @param prior_inclusion Prior inclusion probability for each weight in the layer.
+#' @description Includes function for forward pass, where one can
+#' either use the full model, or the medium probability model (MPM).
+#' Also contains method to initialize parameters and compute KL-divergence.
+#' @examples
+#' l1 <- LBBNN_Linear(in_features = 10,out_features = 5,prior_inclusion = 0.25)
+#' x <- torch_rand(20,10,requires_grad = FALSE)
+#' output <- l1(x,MPM = FALSE) #the forward pass, output has shape (20,5)
+#' print(l1$kl_div()$item()) #compute KL-divergence after the forward pass
+#' @export
+LBBNN_Linear <- nn_module(
+  "LBBNN_Linear",
+  initialize = function(in_features, out_features,prior_inclusion,device) {
+    self$in_features  <- in_features
+    self$out_features <- out_features
+    
+    #weight variational parameters
+    self$weight_mean <- nn_parameter(torch_empty(out_features, in_features))
+    self$weight_rho <- nn_parameter(torch_empty(out_features, in_features))
+    self$weight_sigma <- torch_empty(out_features, in_features)
+    
+    #bias variational parameters 
+    self$bias_mean <- nn_parameter(torch_empty(out_features))
+    self$bias_rho <- nn_parameter(torch_empty(out_features))
+    self$bias_sigma <- torch_empty(out_features)
+    
+    #inclusion variational parameters
+    self$lambda_l <- nn_parameter(torch_empty(out_features, in_features))
+    self$alpha <- torch_empty(out_features, in_features)
+    
+    #define priors. For now, the user is only allowed to define the inclusion prior themselves
+    self$alpha_prior <- alpha_prior(prior_inclusion,out_features,in_features,device)
+    
+    #standard normal prior on the weights and biases
+    self$weight_mean_prior <- torch_zeros(out_features, in_features, device=device)
+    self$weight_sigma_prior <- torch_zeros(out_features, in_features, device=device) + 1 
+    self$bias_mean_prior <- torch_zeros(out_features, device=device)
+    self$bias_sigma_prior <- torch_zeros(out_features, device=device) + 1
+    
+    
+    
+    self$reset_parameters()
+  },
+  reset_parameters = function() {
+    nn_init_normal_(self$weight_mean,mean = 0,std = 1)
+    nn_init_normal_(self$weight_rho,mean = -9, std = 0.1)
+    nn_init_uniform_(self$bias_mean,-0.2,0.2)
+    nn_init_normal_(self$bias_rho,mean = -9, std = 0.1)
+    nn_init_uniform_(self$lambda_l,-10,10)
+    
+    
+  },
+  forward = function(input,MPM=FALSE) {
+    self$alpha <- 1 / (1 + torch_exp(-self$lambda_l))
+    self$weight_sigma <- torch_log1p(torch_exp(self$weight_rho))
+    self$bias_sigma <- torch_log1p(torch_exp(self$bias_rho))
+    
+    
+    if (! MPM) {#compute the mean and the variance of the activations using the LRT
+      e_w <- self$weight_mean * self$alpha
+      var_w <- self$alpha * (self$weight_sigma^2 + (1 - self$alpha) * self$weight_mean^2)
+      
+      e_b <- torch_matmul(input, torch_t(e_w)) + self$bias_mean
+      var_b <- torch_matmul(input^2, torch_t(var_w)) + self$bias_sigma^2
+      eps <- torch_randn(size=(dim(var_b)), device=device)
+      activations <- e_b + torch_sqrt(var_b) * eps
+      
+    }else {#only sample from weights with inclusion prob > 0.5 aka the median probability model 
+      gamma <-(torch_clone(self$alpha)> 0.5) * 1.
+      w <- torch_normal(self$weight_mean, self$weight_sigma)
+      bias <- torch_normal(self$bias_mean, self$bias_sigma)
+      weight <- w * gamma
+      activations <- torch_matmul(input, torch_t(weight)) + bias
+    }
+    
+    
+    
+    return(activations)},
+  kl_div = function() {
+    kl_bias <- torch_sum(torch_log(self$bias_sigma_prior / self$bias_sigma) - 0.5 + (self$bias_sigma^2
+                                                                                     + (self$bias_mean - self$bias_mean_prior)^2) / (2 * self$bias_sigma_prior^2))
+    
+    kl_weight <- torch_sum(self$alpha * (torch_log(self$weight_sigma_prior / self$weight_sigma)
+                                         - 0.5 + torch_log(self$alpha / self$alpha_prior)
+                                         + (self$weight_sigma^2 + (self$weight_mean - self$weight_mean_prior)^2) / (
+                                           2 * self$weight_sigma_prior^2))
+                           + (1 - self$alpha) * torch_log((1 - self$alpha) / (1 - self$alpha_prior)))
+    
+    
+    return(kl_bias + kl_weight)}
+  
+  
+)
+
+
+
