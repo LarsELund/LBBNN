51a981b0426bc9ce70f842b9e764ec421430c345
aliaksah <math.uio.no>
2025-08-14 13:57:39 +0200

    auto_mpg example updated

--- tests_current/regression_auto_mpg.R
+++ tests_current/regression_auto_mpg.R
@@ -33,7 +33,7 @@ ground_truth <- test$outcome
 problem <- 'regression'
 sizes <- c(23,10,1) #7 input variables, one hidden layer of 100 neurons, 1 output neuron.
 inclusion_priors <-c(0.5,0.5) #one prior probability per weight matrix.
-stds <- c(0.1,1) #prior standard deviation for each layer.
+stds <- c(1,10) #prior standard deviation for each layer.
 
 
 #note, having init from (0,5) seeems to reduce it to a linear model with around 83 % acc, similar to what the 
