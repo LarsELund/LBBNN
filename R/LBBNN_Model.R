c88bab59ba6a54c966d42c3e957e47c76d22907b
aliaksah <math.uio.no>
2025-10-06 14:45:18 +0200

    fixed the input skip chek bug

--- R/LBBNN_Model.R
+++ R/LBBNN_Model.R
@@ -189,7 +189,7 @@ LBBNN_Net <- torch::nn_module(
     
   },
   compute_paths_input_skip = function(){
-    if(model$input_skip == FALSE){
+    if(self$input_skip == FALSE){
       stop('model$input_skip must be TRUE to use this funciton')
     }
  
