0be6909b150bc3c18f26cfa853c42106d4a85804
LarsELund <larselnd@gmail.com>
2026-01-19 14:14:42 +0100

    added windows action to check kmnist shape

---
+++ tests/testthat/test_windows_batch_shape.R
@@ -0,0 +1,38 @@
+library(testthat)
+library(torch)
+library(torchvision)
+library(LBBNN)
+
+test_that("KMNIST batch has correct shape", {
+  
+  batch_size <- 100
+  dir <- "./dataset/kmnist"
+  
+  # Transform function to ensure channel dimension exists
+  kmnist_transform <- function(x) {
+    x <- torchvision::transform_to_tensor(x)
+    x
+  }
+  
+  # Load datasets
+  train_ds <- torchvision::kmnist_dataset(
+    dir,
+    download = TRUE,
+    transform = kmnist_transform
+  )
+  
+  train_loader <- torch::dataloader(train_ds, batch_size = batch_size, shuffle = TRUE)
+  
+  # Grab first batch
+  it <- train_loader$.iter()
+  batch <- it$.next()[[1]]
+  
+  print("Batch shape:")
+  print(dim(batch))
+  
+  # Check that batch has shape [batch_size, 1, 28, 28]
+  expect_equal(dim(batch)[1], batch_size)
+  expect_equal(dim(batch)[2], 1)
+  expect_equal(dim(batch)[3], 28)
+  expect_equal(dim(batch)[4], 28)
+})
